c3828d10b9b3b6ad642544fa1a07f88de9c4f24d
DaOZ87 <oberzaucher.david@hakspittal.at>
2025-05-20 09:18:17 +0200

    Klasse davidoberzaucher hinzugefügt

--- Python Kalssenvererbubg/main.py
+++ Python Kalssenvererbubg/main.py
@@ -7,5 +7,10 @@ class Person:
           print(f"Hi, ich heiße {self.name}")
 
 #Eigene Klassen
+class davidoberzaucher(Person):
+    def say_hi(self):
+        print(f"Hi, ich heiße David")
 
 #Hauptprogramm
+davidoberzaucher = davidoberzaucher("David")
+davidoberzaucher.say_hi()
