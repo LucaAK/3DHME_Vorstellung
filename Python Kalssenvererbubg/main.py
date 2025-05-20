331fb5d1d171a5eb9828c4255e84e02b728656f5
petko-marko <petkovic.marko@hakspittal.at>
2025-05-20 09:19:26 +0200

    Klasse Marko Petkovic hinzugefügt

--- Python Kalssenvererbubg/main.py
+++ Python Kalssenvererbubg/main.py
@@ -7,5 +7,16 @@ class Person:
           print(f"Hi, ich heiße {self.name}")
 
 #Eigene Klassen
+class markopetkovic(Person):
+    def say_hi(self):
+        print(f"Hi, ich heiße Marko!")
+
+
+
+
+
+
 
 #Hauptprogramm
+markopetkovic = markopetkovic("Marko")
+markopetkovic.say_hi()
